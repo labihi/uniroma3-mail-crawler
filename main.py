6d0b8ad8773263dcbc5adb88e5e8f08509446762
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-10-31 04:20:27 +0100

    first version of the crawler
    currently it wastes a lot of time printing stuff two times since it doesn't print any output info

--- main.py
+++ main.py
@@ -4,14 +4,41 @@ from bs4 import BeautifulSoup
 from login import get_logged_session
 
 
-def crawl(department, session: requests.Session = None):
-    if(session is None):
+def find_mails(department, session: requests.Session = None):
+    output = set()
+
+    if session is None:
         session = get_logged_session(department)
 
-    response = session.get(f"https://{department}.el.uniroma3.it/")
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(response.text)
+    # visiting the homepage
+    soup = BeautifulSoup(session.get(f"https://{department}.el.uniroma3.it/").text, 'html.parser')
+
+    # finding the courses links
+    courses = soup.select('.list-group a.list-group-item-action[data-parent-key="mycourses"]')
+    # mapping them to the partecipants links
+    partecipants_links = map(
+        lambda course: course['href'].replace('course/view.php', 'user/index.php') + "&perpage=10000", courses)
+
+    # looping through partecipant_links (per course)
+    for partecipant_link in partecipants_links:
+        # visiting the page
+        soup = BeautifulSoup(session.get(partecipant_link).text, 'html.parser')
+        # finding the user links and mapping them to the href
+        user_links = map(lambda user: user['href'], soup.select('#participants tbody a'))
+        # looping through users in this course
+        for user_link in user_links:
+            # visiting the page
+            soup = BeautifulSoup(session.get(user_link).text, 'html.parser')
+            # finding all the mails (even tho it should be only one)
+            mails = set(map(lambda mail_link: mail_link.text, soup.select('.profile_tree a[href*="mailto"]')))
+            # printing the new ones to console
+            print(mails.difference(output))
+            # adding them to the output
+            output.union(mails)
+
+    # filtering duplicates before returning
+    return set(output)
 
 
 if __name__ == '__main__':
-    crawl("ingegneria")
+    print(find_mails("ingegneria"))
